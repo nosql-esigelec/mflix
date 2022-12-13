c18cecd8034b3d9e3c59ca15c010dde7960117ae
bricefotzo <brice.fotzo@hotmail.com>
2022-12-13 21:21:46 +0100

    Set connection

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -24,22 +24,3 @@ def test_atlas_setup(config):
     result = get_coll_names(config)
     assert all(x in result for x in ['users', 'comments', 'movies'])
     assert len(result) >= 5
-
-
-@pytest.mark.connection
-def test_basic_movies():
-    (actual, num_found) = get_movies({}, 0, 20)
-    assert num_found == 23530
-    assert(len(list(actual))) == 20
-
-
-@pytest.mark.connection
-def test_search_by_movie_id():
-    actual = get_movie("573a13acf29313caabd29647")
-    assert actual['title'] == 'King Kong'
-
-
-@pytest.mark.connection
-def test_simple_text_search():
-    (actual, _) = get_movies({"$text": {"$search": "King Kong"}}, 0, 20)
-    assert len(list(actual)) >= 4
